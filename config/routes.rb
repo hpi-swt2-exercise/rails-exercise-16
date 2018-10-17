dc3dc6fa792a96e4858ecfb6e8678ca1e319aeed
Christoph Matthies <chrisma@users.noreply.github.com>
2018-10-17 17:40:38 +0200

    Restructure comments in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,17 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
