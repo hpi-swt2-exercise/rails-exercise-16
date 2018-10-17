043fb80114c44d77328edebb54bf20b301059c73
Christoph Matthies <chrisma@users.noreply.github.com>
2018-10-17 16:30:30 +0200

    Needed changes for rails 5 update

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module IntroExercise2015
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
+    config.active_record.sqlite3.represent_boolean_as_integer = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
